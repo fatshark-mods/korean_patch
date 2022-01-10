6f8db51089c665926504dd4f758c647df0a20024
Iskied <bosa113@naver.com>
2022-01-10 19:10:28 +0900

    A lot of changes

--- scripts/mods/VT2Korean/VT2Korean_loader.lua
+++ scripts/mods/VT2Korean/VT2Korean_loader.lua
@@ -1,35 +1,30 @@
-local vt2korean
-
 local vt2_korean_object = {}
 
 vt2_korean_object.init = function()
   dofile("scripts/mods/VT2Korean/core/VT2Korean")
   dofile("scripts/mods/VT2Korean/core/hooks")
 
-  vt2korean = VT2Korean:new()
-  VT2KoreanMod = vt2korean
+  VT2KoreanMod = VT2Korean:new()
 
   dofile("scripts/mods/VT2Korean/VT2Korean_mod")
-  vt2korean.on_init()
+  VT2KoreanMod.on_init()
 end
 
 vt2_korean_object.update = function(dt)
-  vt2korean:core_update()
-  vt2korean.update()
+  VT2KoreanMod:core_update()
 end
 
 vt2_korean_object.on_unload = function()
-  vt2korean.unload_hooks()
-  vt2korean.on_unload()
+  VT2KoreanMod.unload_hooks()
+  VT2KoreanMod.on_unload()
 end
 
 vt2_korean_object.on_reload = function()
-  vt2korean.on_reload()
+  VT2KoreanMod.on_reload()
 end
 
 vt2_korean_object.on_game_state_changed = function(status, state)
-  vt2korean:apply_delayed_hooks(status, state)
-  vt2korean.on_game_state_changed(status, state)
+  VT2KoreanMod:apply_delayed_hooks(status, state)
 end
 
 return vt2_korean_object
--- scripts/mods/VT2Korean/VT2Korean_mod.lua
+++ scripts/mods/VT2Korean/VT2Korean_mod.lua
@@ -1,37 +1,16 @@
 local mod = VT2KoreanMod
 
--- HUGE Is this okay? We will see...
+-- HUGE, Is this okay? We will see...
 local korean_translation_table = dofile("scripts/mods/VT2Korean/korean_translation_table")
 
 local korean_state = true
 
-local original_fonts = Fonts
--- Probably .notdef glyph fixed
-local custom_font_path = "fonts/mods/VT2Korean/KoPubWorld Batang_Pro Medium_subset"
-local custom_fonts = {
-  arial = {custom_font_path, 14, "arial"},
-  arial_masked = {custom_font_path, 14, "arial", Gui.Masked},
-  arial_write_mask = {custom_font_path, 14, "arial", Gui.WriteMask},
-  hell_shark_arial = {custom_font_path, 14, "arial"},
-  hell_shark_arial_masked = {custom_font_path, 14, "arial", Gui.Masked},
-  hell_shark_arial_write_mask = {custom_font_path, 14, "arial", Gui.WriteMask},
-  hell_shark = {custom_font_path, 20, "gw_body"},
-  hell_shark_masked = {custom_font_path, 20, "gw_body", Gui.Masked},
-  hell_shark_write_mask = {custom_font_path, 20, "gw_body", Gui.WriteMask},
-  hell_shark_header = {custom_font_path, 20, "gw_head"},
-  hell_shark_header_masked = {custom_font_path, 20, "gw_head", Gui.Masked},
-  hell_shark_header_write_mask = {custom_font_path, 20, "gw_head", Gui.WriteMask},
-  chat_output_font = {
-    custom_font_path, 14, "arial", Gui.MultiColor + Gui.ForceSuperSampling + Gui.FormatDirectives
-  },
-  chat_output_font_masked = {
-    custom_font_path, 14, "arial",
-    Gui.MultiColor + Gui.ForceSuperSampling + Gui.FormatDirectives + Gui.Masked
-  }
-}
+-- If Korean Patch is approved, I think Korean chat will be useless
+local original_fonts = table.clone(Fonts)
+local custom_font_path = "fonts/mods/VT2Korean/SourceHanSerifKR-Medium-subset"
 
 --- From UIManager.reload_ingame_ui
-local function reload_sources()
+local function reload_ingame_ui()
   DeadlockStack.pause()
 
   if Managers.ui then
@@ -83,38 +62,21 @@ local function load_custom_store_texture()
   end
 end
 
-local function load_custom_font()
-  Fonts = custom_fonts
-  -- For Korean Chat compatible
-  Fonts.kobatang_arial = original_fonts.arial
-  Fonts.kobatang_chat_output_font = original_fonts.chat_output_font
-end
-
-local function unload_custom_font()
-  Fonts = original_fonts
-  -- For Korean Chat compatible
-  Fonts.kobatang_arial = original_fonts.arial
-  Fonts.kobatang_chat_output_font = original_fonts.chat_output_font
-end
-
 mod.on_init = function()
   load_custom_store_texture()
-  load_custom_font()
-  reload_sources()
+  for _, font in pairs(Fonts) do
+    font[1] = custom_font_path
+  end
+  reload_ingame_ui()
 end
 
 mod.on_unload = function()
-  unload_custom_font()
+  Fonts = original_fonts
+  VMFMod:dump(original_fonts, "original_fonts", 5)
 end
 
 mod.on_reload = function()
-  unload_custom_font()
-end
-
-mod.on_game_state_changed = function(status, state)
-end
-
-mod.update = function()
+  mod.on_unload()
 end
 
 mod:hook(LocalizationManager, "_base_lookup", function(func, self, text_id)
@@ -129,7 +91,7 @@ end)
 
 local text_id_table = {}
 
--- Moded only
+-- Modded only
 if script_data["eac-untrusted"] then
   -- For scraping text_ids
   mod:hook("Localize", function(func, text_id)
@@ -142,9 +104,6 @@ if script_data["eac-untrusted"] then
   end)
 end
 
--- I don't know why, but these isn't localized... maybe widget.style.localize ?
--- {"title_text_victory", "title_text_defeat", "summary_title"} -- Return to keep
-
 -- Weave widget text size should be not reduced
 local scenegraph_id_blacklist = {
   "forge_level_title", "panel_level_title_1", "panel_level_value_1", "panel_level_title_2",
@@ -156,7 +115,7 @@ local scenegraph_id_blacklist = {
   "panel_power_title", "panel_power_value"
 }
 
--- Some text is not localized so
+-- Some text isn't localized, like scoreboard and Return to keep
 mod:hook(UIWidgets, "create_simple_text",
          function(func, text, scenegraph_id, size, color, text_style, optional_font_style, ...)
   local reduce_font_size = 25
